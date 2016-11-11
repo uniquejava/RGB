470f03c13a3fa7a0fcdbdc8a132622e9cefd043d
cyper <345343747@qq.com>
2016-11-12 01:02:15 +0800

    empty lib project with a shared scheme

---
+++ RGB/RGB.h
@@ -0,0 +1,19 @@
+//
+//  RGB.h
+//  RGB
+//
+//  Created by cyper on 12/11/2016.
+//  Copyright © 2016 cyper tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RGB.
+FOUNDATION_EXPORT double RGBVersionNumber;
+
+//! Project version string for RGB.
+FOUNDATION_EXPORT const unsigned char RGBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGB/PublicHeader.h>
+
+
